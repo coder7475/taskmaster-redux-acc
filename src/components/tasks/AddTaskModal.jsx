6249ecfb7699710538e2a1a3a2a4bdb24904c3e8
coder7475 <robiulhossain7475@gmail.com>
2024-03-03 23:42:52 +0600

    feat: create a form with lot of field

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -16,17 +16,68 @@ const AddTaskModal = ({ isOpen, setIsOpen}) => {
               content={"lorem Ipsum so ao"}
         >
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col gap-3">
-                    <label htmlFor="title">Title</label>
+                {/* title */}
+                <div className="flex flex-col mb-5">
+                    <label htmlFor="title" className='mb-2'>Title</label>
                     <input 
                         className="w-full"
                         type="text" 
-                        name="title" 
                         id='title'
-                        {...register('name')} 
+                        {...register('title')} 
                     />
                 </div>
-                <button type='submit' className='p-1 border-2 mt-2 rounded-md hover:bg-slate-600 hover:text-white'>Submit</button>
+                {/* description */}
+                <div className="flex flex-col mb-5">
+                    <label htmlFor="description" className='mb-2'>Description</label>
+                    <input 
+                        className="w-full"
+                        type="text" 
+                        id='description'
+                        {...register('description')} 
+                    />
+                </div>
+                {/* deadline */}
+                <div className="flex flex-col mb-5">
+                    <label htmlFor="date" className='mb-2'>Deadline</label>
+                    <input 
+                        className="w-full"
+                        type="date" 
+                        name="date" 
+                        id='date'
+                        {...register('date')} 
+                    />
+                </div>
+                {/* assigned to */}
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="assignedTo" className='mb-2'>Assign To</label>
+                    <select 
+                        className="w-full rounded-md" 
+                        id="assignedTo"
+                        {...register('assignedTo')}
+                    >
+                        <option value="Mir Hussain">Mir Hossain</option>
+                        <option value="Robiul Hossain">Robiul Hossain</option>
+                        <option value="Nahid Hassan">Nahid Hassan</option>
+
+                    </select>
+                </div>
+                {/* priority */}
+                <div className='flex flex-col mb-5'>
+                    <label htmlFor="priority" className='mb-2'>Assign To</label>
+                    <select 
+                        className="w-full rounded-md" 
+                        id="priority"
+                        {...register('priority')}
+                    >
+                        <option defaultValue value="high">High</option>
+                        <option value="medium">Medium</option>
+                        <option value="low">Low</option>
+                    </select>
+                </div>
+                <div className="flex gap-3 justify-end">
+                    <button type='button' className='btn btn-danger'>Cancel</button>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
+                </div>
             </form>
 
         </Modal>
