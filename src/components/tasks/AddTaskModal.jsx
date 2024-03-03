759a6d0ca09db5d50be6fc74251bd9dd2fe7aadd
coder7475 <robiulhossain7475@gmail.com>
2024-03-03 22:51:25 +0600

    feat: create a simple form

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -16,9 +16,19 @@ const AddTaskModal = ({ isOpen, setIsOpen}) => {
               content={"lorem Ipsum so ao"}
         >
             <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" name="name" {...register('name')} />
-                <button type='submit'>Submit</button>
+                <div className="flex flex-col gap-3">
+                    <label htmlFor="title">Title</label>
+                    <input 
+                        className="w-full"
+                        type="text" 
+                        name="title" 
+                        id='title'
+                        {...register('name')} 
+                    />
+                </div>
+                <button type='submit' className='p-1 border-2 mt-2 rounded-md hover:bg-slate-600 hover:text-white'>Submit</button>
             </form>
+
         </Modal>
     );
 };
