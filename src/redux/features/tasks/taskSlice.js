15a195c267abb07450b42959029006b7d44e470f
coder7475 <robiulhossain7475@gmail.com>
2024-03-07 07:01:35 +0600

    feat: add status to tasks

--- src/redux/features/tasks/taskSlice.js
+++ src/redux/features/tasks/taskSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const tasksSlice = createSlice({
-    name: "tasksSlice",
-    initialState: {
-        tasks: []
+  name: "tasksSlice",
+  initialState: {
+    tasks: [],
+  },
+  reducers: {
+    addTask: (state, { payload }) => {
+      if (state.tasks.length === 0)
+        state.tasks.push({ id: 1, status: "pending", ...payload });
+      else {
+        const lastId = state.tasks.at(-1).id;
+        state.tasks.push({ id: lastId + 1, status: "pending", ...payload });
+      }
     },
-    reducers: {
-        addTask: (state, { payload }) => {
-            if (state.tasks.length === 0)
-                state.tasks.push({ id: 1, ...payload });
-            else {
-                const lastId = state.tasks.at(-1).id;
-                state.tasks.push({ id: lastId + 1, ...payload });
-            }
-        }
-    },
-})
+  },
+});
 
 export const { addTask } = tasksSlice.actions;
 
