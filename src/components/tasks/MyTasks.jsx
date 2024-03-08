2af590591cabb0494dd61f1fe3e10be9e79e2db6
coder7475 <robiulhossain7475@gmail.com>
2024-03-08 19:47:18 +0600

    feat: render the task card dynamically

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -1,38 +1,35 @@
 import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
+import { useSelector } from "react-redux";
+
+function Task({ item }) {
+  return (
+    <div className="bg-secondary/10 rounded-md p-3 flex justify-between">
+      <h1>{item.title}</h1>
+      <div className="flex gap-3">
+        <button className="grid place-content-center" title="Details">
+          <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
+        </button>
+        <button className="grid place-content-center" title="Done">
+          <CheckIcon className="w-5 h-5 text-primary" />
+        </button>
+      </div>
+    </div>
+  );
+}
 
 const MyTasks = () => {
-  const item = {
-    id: 1,
-    status: 'pending',
-    title: 'Remove Button',
-    description:
-      'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
-    date: '2023-08-28',
-    assignedTo: 'Mir Hussain',
-    priority: 'high',
-  };
+  const { tasks } = useSelector((state) => state.tasksSlice);
 
   return (
     <div>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
-        <div
-          key={item.id}
-          className="bg-secondary/10 rounded-md p-3 flex justify-between"
-        >
-          <h1>{item.title}</h1>
-          <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
-              <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
-            </button>
-            <button className="grid place-content-center" title="Done">
-              <CheckIcon className="w-5 h-5 text-primary" />
-            </button>
-          </div>
-        </div>
+        {tasks.map((item) => (
+          <Task key={item.id} item={item}></Task>
+        ))}
       </div>
     </div>
   );
