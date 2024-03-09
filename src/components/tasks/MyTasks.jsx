41fca6e4d262ce483c3df3d93d70e7073eeed12f
coder7475 <robiulhossain7475@gmail.com>
2024-03-09 20:45:45 +0600

    feat: make the task done on clicking done button of my tasks card

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -2,9 +2,10 @@ import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateStatus } from "../../redux/features/tasks/taskSlice";
 
-function Task({ item }) {
+function Task({ item, dispatch }) {
   return (
     <div className="bg-secondary/10 rounded-md p-3 flex justify-between">
       <h1>{item.title}</h1>
@@ -12,7 +13,10 @@ function Task({ item }) {
         <button className="grid place-content-center" title="Details">
           <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
         </button>
-        <button className="grid place-content-center" title="Done">
+        <button 
+          className="grid place-content-center" 
+          title="Done" 
+          onClick={() => dispatch(updateStatus( { id: item.id, status: "done"}))}>
           <CheckIcon className="w-5 h-5 text-primary" />
         </button>
       </div>
@@ -22,13 +26,14 @@ function Task({ item }) {
 
 const MyTasks = () => {
   const { tasks } = useSelector((state) => state.tasksSlice);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
         {tasks.map((item) => (
-          <Task key={item.id} item={item}></Task>
+          <Task key={item.id} item={item} dispatch={dispatch}></Task>
         ))}
       </div>
     </div>
